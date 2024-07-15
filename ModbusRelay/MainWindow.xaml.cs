410dc582519a6b971416727607525daa33c14b9c
DENIS <grachovv12345@gmail.com>
2024-07-15 23:01:08 +0300

    fixes

--- ModbusRelay/MainWindow.xaml.cs
+++ ModbusRelay/MainWindow.xaml.cs
@@ -19,51 +19,11 @@ namespace ModbusRelay
             PortCB.ItemsSource = SerialPort.GetPortNames();
         }
 
-        private void SwithRelay_clik1(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                byte adr = Convert.ToByte(AdressBox.Text);
-                int rate = Convert.ToInt32(BaudRateCB.Text);
-                string port = PortCB.Text;
-
-                if ((bool)R1on.IsChecked) WriteSerial(port, rate, adr, 0, 0xFF);
-                if ((bool)R1off.IsChecked) WriteSerial(port, rate, adr, 0, 0);
-            }
-            catch (Exception ex)
-            {
-                OutputList.Items.Add($"err: {ex.Message}");
-            }
-        }
-
-        private void SwithRelay_clik2(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                byte adr = Convert.ToByte(AdressBox.Text);
-                int rate = Convert.ToInt32(BaudRateCB.Text);
-                string port = PortCB.Text;
-
-                if ((bool)R2on.IsChecked) WriteSerial(port, rate, adr, 1, 0xFF);
-                if ((bool)R2off.IsChecked) WriteSerial(port, rate, adr, 1, 0);
-            }
-            catch (Exception ex)
-            {
-                OutputList.Items.Add($"err: {ex.Message}");
-            }
-        }
-
-
         private void SwithRelay1_click(object sender, RoutedEventArgs e)
         {
             if (sender is Button btn)
             {
                 SwithRelay(btn, 0);
-                switch (btn.Content.ToString())
-                {
-                    case "ON": R1on.IsChecked = true; break;
-                    case "OFF": R1off.IsChecked = true; break;
-                }
             }
         }
 
@@ -72,11 +32,6 @@ namespace ModbusRelay
             if (sender is Button btn)
             {
                 SwithRelay(btn, 1);
-                switch (btn.Content.ToString())
-                {
-                    case "ON": R2on.IsChecked = true; break;
-                    case "OFF": R2off.IsChecked = true; break;
-                }
             }
         }
 
@@ -87,131 +42,31 @@ namespace ModbusRelay
                 byte adr = Convert.ToByte(AdressBox.Text);
                 int rate = Convert.ToInt32(BaudRateCB.Text);
                 string port = PortCB.Text;
+                string result;
+
+                ModBus modBus = new ModBus(port, rate);
 
                 if (btn.Content.ToString() == "OFF")
                 {
-                    WriteSerial(port, rate, adr, Nrelay, 0xFF);
+                    result = modBus.Write($"{adr.ToString("X2")} 05 00 {Nrelay} FF 00");
                     btn.Content = "ON";
                     btn.Background = new SolidColorBrush(Colors.Green);
                 }
                 else
                 {
-                    WriteSerial(port, rate, adr, Nrelay, 0);
+                    result =  modBus.Write($"{adr.ToString("X2")} 05 00 {Nrelay} 00 00");
                     btn.Content = "OFF";
                     btn.Background = null;
                 }
+
+                OutputList.Text += $"<- : {result}\n" +
+                                   $" ->: {modBus.Read()}\n";
+                modBus.Dispose();
             }
             catch (Exception ex)
             {
-                OutputList.Items.Add($"err: {ex.Message}");
+                OutputList.Text += $"err: {ex.Message}\n";
             }
-        }
-
-        private void WriteSerial(string comport, int baudrate, string byteString)
-        {
-            using (var port = new SerialPort(comport))
-            {
-                port.BaudRate = baudrate;
-                port.Parity = Parity.None;
-                port.DataBits = 8;
-                port.StopBits = StopBits.One;
-                port.ReadTimeout = Properties.Settings.Default.TimeOut;
-                port.WriteTimeout = Properties.Settings.Default.TimeOut;
-
-                try
-                {
-                    port.Open();
-
-                    // Convert the string to a byte array
-                    byte[] request = byteString.Split(' ').Select(s => Convert.ToByte(s, 16)).ToArray();
-
-                    // Calculate CRC
-                    byte[] crc = CalculateCRC(request);
-
-                    // Add CRC to the request
-                    byte[] fullRequest = new byte[request.Length + 2];
-                    Array.Copy(request, fullRequest, request.Length);
-                    fullRequest[request.Length] = crc[0];
-                    fullRequest[request.Length + 1] = crc[1];
-
-                    // Отправка запроса
-                    OutputList.Items.Add($"<- : {String.Join(" ", fullRequest.Select(num => num.ToString("X2")))}");
-                    port.Write(fullRequest, 0, fullRequest.Length);
-
-                    // Создание буфера для ответа
-                    byte[] buffer = new byte[8];
-                    int bytesRead = 0;
-                    int totalBytesToRead = buffer.Length;
-                    int attempts = 0;
-
-                    // Чтение данных из последовательного порта
-                    while (bytesRead < totalBytesToRead && attempts < 10)
-                    {
-                        if (port.BytesToRead > 0)
-                        {
-                            bytesRead += port.Read(buffer, bytesRead, totalBytesToRead - bytesRead);
-                        }
-                        else
-                        {
-                            System.Threading.Thread.Sleep(50); // Пауза для ожидания данных
-                            attempts++;
-                        }
-                    }
-
-                    // Проверка результата чтения
-                    if (bytesRead > 0)
-                    {
-                        string crcCheck = CheckCRC(buffer) ? "OK" : "MISMATCH";
-                        OutputList.Items.Add($" ->: {String.Join(" ", buffer.Select(b => b.ToString("X2")))} [CRC {crcCheck}]");
-                    }
-                }
-                catch (TimeoutException)
-                {
-                    OutputList.Items.Add("err: Timeout");
-                }
-                catch (Exception ex)
-                {
-                    OutputList.Items.Add($"err: {ex.Message}");
-                }
-                finally
-                {
-                    if (port.IsOpen) port.Close();
-                }
-            }
-        }
-
-        private void WriteSerial(string comport, int baudrate, byte slaveId, byte Nr, byte Sr)
-        {
-            WriteSerial(comport, baudrate, $"{slaveId.ToString("X2")} 05 00 {Nr} {Sr.ToString("X2")} 00");
-        }
-
-        static byte[] CalculateCRC(byte[] data)
-        {
-            ushort crc = 0xFFFF;
-            foreach (byte b in data)
-            {
-                crc ^= b;
-                for (int i = 0; i < 8; i++)
-                {
-                    if ((crc & 0x0001) != 0)
-                        crc = (ushort)((crc >> 1) ^ 0xA001);
-                    else
-                        crc >>= 1;
-                }
-            }
-
-            // Преобразование 16-битного целого в массив из 2 байт
-            byte[] crcBytes = new byte[2];
-            crcBytes[0] = (byte)(crc & 0xFF); // Младший байт
-            crcBytes[1] = (byte)((crc >> 8) & 0xFF); // Старший байт
-            return crcBytes;
-        }
-
-        static bool CheckCRC(byte[] data)
-        {
-            byte[] receivedCrc = data.Skip(data.Length - 2).Take(2).ToArray();
-            byte[] calculatedCrc = CalculateCRC(data.Take(data.Length - 2).ToArray());
-            return receivedCrc.SequenceEqual(calculatedCrc);
         }
 
         static void OpenBrowser(string url)
@@ -236,13 +91,13 @@ namespace ModbusRelay
             }
             catch (Exception ex)
             {
-                OutputList.Items.Add($"err: {ex.Message}");
+                OutputList.Text += $"err: {ex.Message}\n";
             }
         }
 
         private void ClearOutList(object sender, RoutedEventArgs e)
         {
-            OutputList.Items.Clear();
+            OutputList.Text = string.Empty;
         }
 
         private void Send_click(object sender, RoutedEventArgs e)
@@ -252,11 +107,14 @@ namespace ModbusRelay
                 int rate = Convert.ToInt32(BaudRateCB.Text);
                 string port = PortCB.Text;
 
-                WriteSerial(port, rate, CommandBox.Text);
+                ModBus modBus = new ModBus(port, rate);
+                OutputList.Text += $"<- : {modBus.Write(CommandBox.Text)}\n" +
+                                   $" ->: {modBus.Read()}\n";
+                modBus.Dispose();
             }
             catch (Exception ex)
             {
-                OutputList.Items.Add($"err: {ex.Message}");
+                OutputList.Text += $"err: {ex.Message}\n";
             }
         }
 
---
+++ ModbusRelay/ModBus.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO.Ports;
+using System.Linq;
+
+namespace ModbusRelay
+{
+    internal class ModBus : IDisposable
+    {
+        private string comport;
+        private int bautrate;
+        private Parity parity = Parity.None;
+        private int dataBits = 8;
+        private StopBits stopBits = StopBits.One;
+        SerialPort port;
+
+        public ModBus(string comport, int bautrate)
+        {
+            this.comport = comport;
+            this.bautrate = bautrate;
+
+            port = new SerialPort
+            {
+                PortName = this.comport,
+                BaudRate = this.bautrate,
+                Parity = this.parity,
+                DataBits = this.dataBits,
+                StopBits = this.stopBits,
+                ReadTimeout = Properties.Settings.Default.TimeOut,
+                WriteTimeout = Properties.Settings.Default.TimeOut
+            };
+            port.Open();
+        }
+
+        public string Write(string bytes)
+        {
+            // Convert the string to a byte array
+            byte[] request = bytes.Split(' ').Select(s => Convert.ToByte(s, 16)).ToArray();
+
+            // Calculate CRC
+            byte[] crc = CalculateCRC(request);
+
+            // Add CRC to the request
+            byte[] fullRequest = new byte[request.Length + 2];
+            Array.Copy(request, fullRequest, request.Length);
+            fullRequest[request.Length] = crc[0];
+            fullRequest[request.Length + 1] = crc[1];
+
+            // Отправка запроса
+            port.Write(fullRequest, 0, fullRequest.Length);
+            return $"{String.Join(" ", fullRequest.Select(num => num.ToString("X2")))}";
+        }
+
+        public string Read()
+        {
+            // Создание буфера для ответа
+            byte[] buffer = new byte[8];
+            int bytesRead = 0;
+            int totalBytesToRead = buffer.Length;
+            int attempts = 0;
+
+            // Чтение данных из последовательного порта
+            while (bytesRead < totalBytesToRead && attempts < 10)
+            {
+                if (port.BytesToRead > 0)
+                {
+                    bytesRead += port.Read(buffer, bytesRead, totalBytesToRead - bytesRead);
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(50); // Пауза для ожидания данных
+                    attempts++;
+                }
+            }
+
+            string crcCheck = CheckCRC(buffer) ? "OK" : "MISMATCH";
+            return $"{String.Join(" ", buffer.Select(b => b.ToString("X2")))} [CRC {crcCheck}]";
+        }
+
+        static byte[] CalculateCRC(byte[] data)
+        {
+            ushort crc = 0xFFFF;
+            foreach (byte b in data)
+            {
+                crc ^= b;
+                for (int i = 0; i < 8; i++)
+                {
+                    if ((crc & 0x0001) != 0)
+                        crc = (ushort)((crc >> 1) ^ 0xA001);
+                    else
+                        crc >>= 1;
+                }
+            }
+
+            // Преобразование 16-битного целого в массив из 2 байт
+            byte[] crcBytes = new byte[2];
+            crcBytes[0] = (byte)(crc & 0xFF); // Младший байт
+            crcBytes[1] = (byte)((crc >> 8) & 0xFF); // Старший байт
+            return crcBytes;
+        }
+
+        static bool CheckCRC(byte[] data)
+        {
+            byte[] receivedCrc = data.Skip(data.Length - 2).Take(2).ToArray();
+            byte[] calculatedCrc = CalculateCRC(data.Take(data.Length - 2).ToArray());
+            return receivedCrc.SequenceEqual(calculatedCrc);
+        }
+
+        public void Dispose()
+        {
+            port.Close();
+            port.Dispose();
+        }
+    }
+}
